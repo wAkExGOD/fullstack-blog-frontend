770379d01b943b192813deef2001b21b49b99338
Vlad Panasik <vladpanasik@mail.ru>
2022-06-26 18:07:44 +0300

    First commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import Button from '@mui/material/Button';
+import { useSelector, useDispatch } from "react-redux";
+
+import styles from './Header.module.scss';
+import Container from '@mui/material/Container';
+import { logout, selectIsAuth } from "../../redux/slices/auth";
+
+export const Header = () => {
+  const dispatch = useDispatch();
+  const isAuth = useSelector(selectIsAuth);
+
+  const onClickLogout = () => {
+    if (window.confirm('Вы действительно хотите выйти из аккаунта?')) {
+      dispatch(logout());
+      window.localStorage.removeItem('token');
+    }
+  };
+
+  return (
+    <div className={styles.root}>
+      <Container maxWidth="lg">
+        <div className={styles.inner}>
+          <Link className={styles.logo} to="/">
+            <div>ARCHAKOV BLOG</div>
+          </Link>
+          <div className={styles.buttons}>
+            {isAuth ? (
+              <>
+                <Link to="/add-post">
+                  <Button variant="contained">Написать статью</Button>
+                </Link>
+                <Button onClick={onClickLogout} variant="contained" color="error">
+                  Выйти
+                </Button>
+              </>
+            ) : (
+              <>
+                <Link to="/login">
+                  <Button variant="outlined">Войти</Button>
+                </Link>
+                <Link to="/register">
+                  <Button variant="contained">Создать аккаунт</Button>
+                </Link>
+              </>
+            )}
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
---
+++ src/pages/AddPost/index.jsx
@@ -0,0 +1,147 @@
+import React from 'react';
+import { useSelector } from "react-redux";
+import { useNavigate, Navigate, useParams } from "react-router-dom";
+import TextField from '@mui/material/TextField';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+import SimpleMDE from 'react-simplemde-editor';
+import axios from '../../axios';
+
+import 'easymde/dist/easymde.min.css';
+import styles from './AddPost.module.scss';
+import { selectIsAuth } from "../../redux/slices/auth";
+
+export const AddPost = () => {
+  const {id} = useParams();
+  const navigate = useNavigate();
+  const isAuth = useSelector(selectIsAuth);
+  const [isLoading, setLoading] = React.useState(false);
+  const [text, setText] = React.useState('');
+  const [title, setTitle] = React.useState('');
+  const [tags, setTags] = React.useState('');
+  const [imageUrl, setImageUrl] = React.useState('');
+  const inputFileRef = React.useRef(null); 
+
+  const handleChangeFile = async (event) => {
+    try {
+      const formData = new FormData();
+      const file = event.target.files[0];
+      formData.append('image', file);
+      const { data } = await axios.post('/upload', formData);
+      setImageUrl(data.url);
+    } catch (err) {
+      console.warn(err);
+      alert('Ошибка при загрузке файла!');
+    }
+  };
+
+  const onClickRemoveImage = () => {
+    setImageUrl('');
+  };
+
+  const onChange = React.useCallback((value) => {
+    setText(value);
+  }, []);
+
+  const onSubmit = async () => {
+    try {
+      setLoading(true);
+
+      const fields = {
+        title,
+        imageUrl,
+        tags,
+        text,
+      };
+
+      const { data } = isEditing
+        ? await axios.patch(`/posts/${id}`, fields)
+        : await axios.post('/posts', fields);
+
+      const _id = isEditing ? id : data._id;
+
+      navigate(`/posts/${_id}`);
+    } catch (err) {
+      console.warn(err);
+      alert('Ошибка при создании статьи!');
+    }
+  }
+
+  const isEditing = Boolean(id);
+
+  React.useEffect(() => {
+    if (id) {
+      axios.get(`/posts/${id}`).then(({data: {title, text, imageUrl, tags}}) => {
+        setTitle(title);
+        setText(text);
+        setImageUrl(imageUrl);
+        setTags(tags.join(','));
+      }).catch((err) => {
+        console.warn(err);
+        alert('Ошибка при загрузке статьи!');
+      });
+    }
+  }, []);
+
+  const options = React.useMemo(
+    () => ({
+      spellChecker: false,
+      maxHeight: '400px',
+      autofocus: true,
+      placeholder: 'Введите текст...',
+      status: false,
+      autosave: {
+        enabled: true,
+        delay: 1000,
+      },
+    }),
+    [],
+  );
+
+  if (!window.localStorage.getItem('token') && !isAuth) {
+    return <Navigate to="/" />;
+  }
+
+  return (
+    <Paper style={{ padding: 30 }}>
+      <Button onClick={() => inputFileRef.current.click()} variant="outlined" size="large">
+        Загрузить превью
+      </Button>
+      <input ref={inputFileRef} type="file" onChange={handleChangeFile} hidden />
+      {imageUrl && (
+        <>
+          <Button variant="contained" color="error" onClick={onClickRemoveImage}>
+            Удалить
+          </Button>
+          <img className={styles.image} src={`http://localhost:4444${imageUrl}`} alt="Uploaded" />
+        </>
+      )}
+      <br />
+      <br />
+      <TextField
+        classes={{ root: styles.title }}
+        variant="standard"
+        placeholder="Заголовок статьи..."
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        fullWidth
+      />
+      <TextField
+        classes={{ root: styles.tags }}
+        variant="standard" placeholder="Тэги"
+        value={tags}
+        onChange={(e) => setTags(e.target.value)}
+        fullWidth
+      />
+      <SimpleMDE className={styles.editor} value={text} onChange={onChange} options={options} />
+      <div className={styles.buttons}>
+        <Button onClick={onSubmit} size="large" variant="contained">
+          {isEditing ? 'Сохранить' : 'Опубликовать'}
+        </Button>
+        <a href="/">
+          <Button size="large">Отмена</Button>
+        </a>
+      </div>
+    </Paper>
+  );
+};
